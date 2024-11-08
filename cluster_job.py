a13dcce603a036d8de9fb741357e12834848a664
bn_platform <bertrandnouvel@bmlltech.com>
2024-11-08 14:19:33 +0000

    updating cluster job

--- cluster_job.py
+++ cluster_job.py
@@ -1,5 +1,6 @@
 from bcdf.integrations.rclone import RCloneSyncer
 from bcdf.analytics import schedulable_job
+import os
 
 #Running as hadoop
 CUSTOM_BOOTSTRAP="""
@@ -15,7 +16,7 @@ def main(date):
     print("docdir = ", docdir)
     # configure custom pandoc env
     with open(os.path.expanduser("~/bin/pandoc"),'w') as f:
-        f.write('#!/bin/bash\nPANDOC_DATA_DIR='"$HOME"'/.pandoc/data  '"$HOME"'/local/usr/bin/pandoc --data-dir='"$HOME"'/.pandoc/data  --abbreviations='"$HOME"'/.pandoc/data/abbreviations $*')
+        f.write('#!/bin/bash\nPANDOC_DATA_DIR='"$HOME"'/.pandoc/data /usr/bin/pandoc --data-dir='"$HOME"'/.pandoc/data  --abbreviations='"$HOME"'/.pandoc/data/abbreviations $*')
     os.system("chmod +x ~/bin/pandoc")
     os.system("cp -r /usr/share/pandoc/ ~/.local/share/")
     os.system("ln -s $PWD/template.tex ~/.local/share/pandoc/data/templates/bmll_template.latex")
