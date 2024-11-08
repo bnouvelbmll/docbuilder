11f15d5cb6d86a60f3859b585698ba71b3ec1f4f
bn_platform <bertrandnouvel@bmlltech.com>
2024-11-08 13:05:19 +0000

    cluster_job

--- cluster_job.py
+++ cluster_job.py
@@ -1,5 +1,5 @@
 from bcdf.integrations.rclone import RCloneSyncer
-from bcdf import bmll_scheduled_job
+from bcdf.analytics import schedulable_job
 
 #Running as hadoop
 CUSTOM_BOOTSTRAP="""
@@ -8,7 +8,7 @@ sudo usermod -a -G docker yarn
 """
 
 # Running as yarn
-@bmll_scheduled_job(frequency="@weekly", cluster_name="doc-generator", bootstrap=CUSTOM_BOOTSTRAP)
+@schedulable_job(frequency="@weekly", cluster_name="doc-generator", bootstrap=CUSTOM_BOOTSTRAP)
 def main(date):
     import bmll2
     docdir=os.getcwd()
@@ -17,16 +17,13 @@ def main(date):
     with open(os.path.expanduser("~/bin/pandoc"),'w') as f:
         f.write('#!/bin/bash\nPANDOC_DATA_DIR='"$HOME"'/.pandoc/data  '"$HOME"'/local/usr/bin/pandoc --data-dir='"$HOME"'/.pandoc/data  --abbreviations='"$HOME"'/.pandoc/data/abbreviations $*')
     os.system("chmod +x ~/bin/pandoc")
-    os.system("cp -r /usr/share/pandoc/ ~/.local/share/)
+    os.system("cp -r /usr/share/pandoc/ ~/.local/share/")
     os.system("ln -s $PWD/template.tex ~/.local/share/pandoc/data/templates/bmll_template.latex")
 
-    
     # run and build tje documentation
-    os.system("INTERNAL_DOCS=0 python update_docs.py")              
+    os.system("INTERNAL_DOCS=0 python update_docs.py")
     rc=RCloneSyncer(os.path.join(docdir,'render'), "product_team/docs", area="organisation")
-              
-    os.system("INTERNAL_DOCS=1 python update_docs.py")
+    os.system("rm -r render;INTERNAL_DOCS=1 python update_docs.py")
     rc=RCloneSyncer(os.path.join(docdir,'render'), "product_team/internal-docs", area="organisation")
-              
     rc.upload()
-    
+
